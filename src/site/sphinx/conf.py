dc158992d2be5f092a9d3a4ccd483bd5f73948eb
Ashley Engelund (weedySeaDragon @ github) <registrations@ashleycaroline.com>
2019-02-28 20:27:19 -0800

    trying current file dir + printing out plantuml from conf.py

--- src/site/sphinx/conf.py
+++ src/site/sphinx/conf.py
@@ -95,8 +95,10 @@ extensions = [
 #plantuml = 'java -Djava.awt.headless=true -jar plantuml.jar'
 #plantuml = 'java -jar /Users/ashleyengelund/dev/java/plantuml/plantuml.jar'
 
-# per https://trustin.github.io/sphinx-maven-plugin/
-plantuml = os.getenv('plantuml')
+plantuml = 'java -Djava.awt.headless=true -jar %s' % os.path.join(os.path.dirname(__file__), "plantuml.jar")
+
+print(' >>> plantuml =' + plantuml)
+
 
 # ------------------------
 # recommonMark
